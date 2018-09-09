4a1d613f973e626966446c678d4462c468351c67
martinusbe <martinusbe@gmail.com>
2018-09-09 10:20:22 +0200

    Revert "msm8998: disable dexpreopt"
    This reverts commit 3df3b0f175a834b1a104aa87d89f97bdab3ecab8.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -209,8 +209,15 @@ OVERRIDE_RS_DRIVER:= libRSDriver_adreno.so
 VSYNC_EVENT_PHASE_OFFSET_NS := 2000000
 SF_VSYNC_EVENT_PHASE_OFFSET_NS := 6000000
 
-# dexpreopt
-WITH_DEXPREOPT := false
+# Enable dexpreopt to speed boot time
+ifeq ($(HOST_OS),linux)
+  ifneq ($(TARGET_BUILD_VARIANT),eng)
+    ifeq ($(WITH_DEXPREOPT),)
+      WITH_DEXPREOPT := true
+      WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := true
+    endif
+  endif
+endif
 
 # GPS
 TARGET_NO_RPC := true
